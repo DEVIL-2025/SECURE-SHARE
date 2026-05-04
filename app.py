386ef658a8ecd1f597cddc89bcba7947e3177d25
Harsh Vardhan <your-email@gmail.com>
2026-05-04 18:27:05 +0530

    prepare for render deployment

--- app.py
+++ app.py
@@ -753,7 +753,9 @@ def init_db():
 
 init_db()
 
+
 if __name__ == '__main__':
     init_db()
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
     
